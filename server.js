40bb7305a6a79e2029e24b8e7cc04a6e536b08e6
Jonie <joniebariaDE@gmail.com>
2025-03-19 04:19:39 +0800

    peerhost

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const express = require("express");
-const app = express();
-const server = require("http").Server(app);
-const io = require("socket.io")(server);
 const { v4: uuidV4 } = require("uuid");
+const { ExpressPeerServer } = require("peer");
+const http = require("http");
+const socketIo = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+
+// Initialize PeerJS server
+const peerServer = ExpressPeerServer(server, {
+  path: "/peerjs",
+});
+app.use("/peerjs", peerServer);
 
 app.get("/", (req, res) => {
   res.redirect(`/${uuidV4()}`);
@@ -21,9 +31,12 @@ io.on("connection", (socket) => {
     socket.to(roomId).broadcast.emit("user-connected", userId);
 
     socket.on("disconnect", () => {
-      socket.to(roomId).broadcast.emit("user-disconnected", userId);
+      socket.to(roomId).emit("user-disconnected", userId);
     });
   });
 });
 
-server.listen(3000);
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
